2f3a519ed33c0bcd1f9af0c5f387d1d9bf4b2149
Janne Niinisalo <janne.niinisalo@gmail.com>
2025-04-22 10:54:46 +0300

    Add touch/tap control to tagitem (no real function yet)

--- nexecute/lib/home/widgets/taglistitem.dart
+++ nexecute/lib/home/widgets/taglistitem.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nexecute/models/tag.dart';
+import 'package:nexecute/shared/styles.dart';
 
 class TagListItem extends StatelessWidget {
   final Tag tag;
@@ -9,15 +10,19 @@ class TagListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 3.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.blueGrey,
-          border: Border.all(color: Colors.blueGrey),
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(child: Text(tag.name)),
+      child: InkWell(
+        onTap: () {
+          print('tag tapped');
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: darkCyan,
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(child: Text(tag.name)),
+          ),
         ),
       ),
     );
