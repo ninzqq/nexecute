637207200322b7c44ac05cdd84800213c33b2a7c
Janne Niinisalo <janne.niinisalo@gmail.com>
2026-08-30 22:42:15 +0300

    Upgrade android toolchain to follow up-to-date recommendations

--- nexecute/android/app/build.gradle.kts
+++ nexecute/android/app/build.gradle.kts
@@ -19,10 +19,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.jndevworks.nexecute"
@@ -41,6 +37,12 @@ android {
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
